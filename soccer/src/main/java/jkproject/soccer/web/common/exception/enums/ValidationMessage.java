2121103e4204c3574b37b41dbc2585d50949ca42
ljkhyeong <jolri24@gmail.com>
2023-12-10 07:55:13 +0900

    refactor : 어노테이션 옵션에 enum을 넣기 위해 기존 enum 수정
    어노테이션은 컴파일 타임 상수만 옵션으로 받을 수 있기 때문에 런타임 시점에 getter 호출 시 생성자를 통해 값이 정해진 후 반환되는 enum getMessage는 넣을 수 없다.

--- soccer/src/main/java/jkproject/soccer/web/common/exception/enums/ValidationMessage.java
+++ soccer/src/main/java/jkproject/soccer/web/common/exception/enums/ValidationMessage.java
@@ -2,14 +2,18 @@ package jkproject.soccer.web.common.exception.enums;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
 public enum ValidationMessage {
 
-	CANNOT_BE_NULL("필수로 입력되어야 합니다."),
-	UNMATCHED_LOGINID("아이디는 4자 이상, 15자 이하의 영문자 혹은 영문자와 숫자의 조합이어야 합니다.");
+	CANNOT_BE_NULL(Messages.NOT_NULL),
+	UNMATCHED_LOGINID(Messages.LOGIN_REQUIREMENTS);
+
+	public static class Messages {
+		public static final String NOT_NULL = "필수로 입력되어야 합니다.";
+		public static final String LOGIN_REQUIREMENTS = "아이디는 4자 이상, 15자 이하의 영문자 혹은 영문자와 숫자의 조합이어야 합니다.";
+	}
 
 	private final String message;
 
