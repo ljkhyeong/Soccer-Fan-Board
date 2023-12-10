1c1fe97dc268dfd139f51fc6f43284369464d501
ljkhyeong <jolri24@gmail.com>
2023-12-10 10:25:12 +0900

    feat : jwt 토큰 발급, 검증 클래스 추가

--- soccer/src/main/java/jkproject/soccer/domain/repository/user/UserRepository.java
+++ soccer/src/main/java/jkproject/soccer/domain/repository/user/UserRepository.java
@@ -1,12 +1,12 @@
 package jkproject.soccer.domain.repository.user;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import jkproject.soccer.domain.entity.user.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	boolean existsByLoginId(String loginId);
 
-	boolean existsByEmail(String email);
-
+	Optional<User> findByLoginId(String loginId);
 }
