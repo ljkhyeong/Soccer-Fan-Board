08cef0441f2c3c8e2951e135358666a42c302ffb
ljkhyeong <jolri24@gmail.com>
2023-12-11 19:30:26 +0900

    feat: 요청에서 jwt 추출, 조회 후 인증 정보를 부여하는 커스텀 필터 추가

--- soccer/src/main/java/jkproject/soccer/web/common/exception/enums/ErrorCode.java
+++ soccer/src/main/java/jkproject/soccer/web/common/exception/enums/ErrorCode.java
@@ -10,7 +10,9 @@ import lombok.Getter;
 public enum ErrorCode {
 
 	NON_EXISTENT_USER_ID(HttpStatus.BAD_REQUEST, "존재하지 않는 회원"),
-	NON_EXISTENT_POST_ID(HttpStatus.BAD_REQUEST, "존재하지 않는 게시물");
+	NON_EXISTENT_POST_ID(HttpStatus.BAD_REQUEST, "존재하지 않는 게시물"),
+	NOT_FOUND_TOKEN(HttpStatus.UNAUTHORIZED, "토큰을 찾을 수 없습니다."),
+	INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰");
 	// TODO 에러코드와 에러 메시지
 
 	private final HttpStatus httpStatus;
