52a61ab27f3acedabe296971a25e4d0c867a5012
ljkhyeong <jolri24@gmail.com>
2023-12-12 20:09:41 +0900

    feat : 로그인 기능 추가
    jwt 인증 후 쿠키로 생성, response에 담아서 전달

--- soccer/src/main/java/jkproject/soccer/api/controller/auth/AuthController.java
+++ soccer/src/main/java/jkproject/soccer/api/controller/auth/AuthController.java
@@ -1,2 +1,40 @@
-package jkproject.soccer.api.controller.auth;public class AuthController {
+package jkproject.soccer.api.controller.auth;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import jkproject.soccer.api.common.dto.response.Response;
+import jkproject.soccer.api.dto.auth.request.LoginRequestDto;
+import jkproject.soccer.api.dto.auth.response.LoginResponseDto;
+import jkproject.soccer.web.auth.config.jwt.TokenType;
+import jkproject.soccer.web.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+	private final AuthService authService;
+
+	@PostMapping("/login")
+	public Response<Void> login(@RequestBody @Valid LoginRequestDto requestDto, HttpServletResponse response) {
+		LoginResponseDto responseDto = authService.login(requestDto);
+
+		addTokenCookie(response, responseDto.getAccessToken(), TokenType.ACCESS);
+		addTokenCookie(response, responseDto.getRefreshToken(), TokenType.REFRESH);
+
+		return Response.success();
+	}
+
+	private void addTokenCookie(HttpServletResponse response, String token, TokenType tokenType) {
+		ResponseCookie tokenCookie = tokenType.createCookieFrom(token);
+		response.addHeader(HttpHeaders.SET_COOKIE, tokenCookie.toString());
+	}
 }
--- soccer/src/main/java/jkproject/soccer/web/auth/service/AuthService.java
+++ soccer/src/main/java/jkproject/soccer/web/auth/service/AuthService.java
@@ -1,2 +1,60 @@
-package jkproject.soccer.web.auth.service;public class AuthService {
+package jkproject.soccer.web.auth.service;
+
+import org.springframework.context.ApplicationContextException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jkproject.soccer.api.dto.auth.request.LoginRequestDto;
+import jkproject.soccer.api.dto.auth.response.LoginResponseDto;
+import jkproject.soccer.domain.entity.user.User;
+import jkproject.soccer.domain.repository.user.UserRepository;
+import jkproject.soccer.web.auth.config.jwt.JwtTokenProvider;
+import jkproject.soccer.web.auth.config.jwt.TokenType;
+import jkproject.soccer.web.auth.repository.RefreshTokenRepository;
+import jkproject.soccer.web.common.exception.enums.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class AuthService {
+
+	private final PasswordEncoder passwordEncoder;
+	private final JwtTokenProvider jwtTokenProvider;
+	private final RefreshTokenRepository refreshTokenRepository;
+	private final UserRepository userRepository;
+
+	public LoginResponseDto login(LoginRequestDto requestDto) {
+
+		User user = validateLoginIdAndPassword(requestDto);
+		String loginId = user.getLoginId();
+
+		String accessToken = jwtTokenProvider.generateToken(loginId, TokenType.ACCESS);
+		String refreshToken = refreshTokenRepository.findRefreshTokenByLoginId(loginId)
+			.orElseGet(() -> createAndSaveRefreshToken(loginId));
+		
+		return LoginResponseDto.builder()
+			.accessToken(accessToken)
+			.refreshToken(refreshToken)
+			.build();
+	}
+
+	private User validateLoginIdAndPassword(LoginRequestDto requestDto) {
+		User user = userRepository.findByLoginId(requestDto.getLoginId())
+			.orElseThrow(() -> new ApplicationContextException(ErrorCode.NON_EXISTENT_USER_ID.getMessage()));
+
+		if (passwordEncoder.matches(user.getPassword(), requestDto.getPassword())) {
+			throw new ApplicationContextException(ErrorCode.INVALID_PASSWORD.getMessage());
+		}
+		return user;
+	}
+
+	private String createAndSaveRefreshToken(String loginId) {
+		String refreshToken = jwtTokenProvider.generateToken(loginId, TokenType.REFRESH);
+		refreshTokenRepository.save(loginId, refreshToken);
+		return refreshToken;
+	}
 }
