761890bd0d6945bc86c26bd42aec9874d68626ad
ljkhyeong <jolri24@gmail.com>
2023-12-20 22:22:44 +0900

    refactor: BaseTimeEntity 누락부분 추가

--- soccer/src/main/java/jkproject/soccer/domain/entity/BaseTimeEntity.java
+++ soccer/src/main/java/jkproject/soccer/domain/entity/BaseTimeEntity.java
@@ -6,18 +6,23 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
 public class BaseTimeEntity {
 
 	@CreatedDate
+	@Column(updatable = false, name = "created_at")
 	private LocalDateTime createdAt;
 
 	@LastModifiedDate
+	@Column(name = "modified_at")
 	private LocalDateTime modifiedAt;
 }
