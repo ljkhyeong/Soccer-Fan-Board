9e268f32c737d0f9d1445f94337d47239828f2cd
ljkhyeong <jolri24@gmail.com>
2023-12-20 22:23:20 +0900

    refactor: CommentRepository 메소드 쿼리 커스텀

--- soccer/src/main/java/jkproject/soccer/domain/repository/board/comment/CommentRepository.java
+++ soccer/src/main/java/jkproject/soccer/domain/repository/board/comment/CommentRepository.java
@@ -3,9 +3,11 @@ package jkproject.soccer.domain.repository.board.comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import jkproject.soccer.domain.entity.board.comment.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+	@Query("select c from Comment c where c.post.postId = :postId")
 	Page<Comment> findByPostId(Long postId, Pageable pageable);
 }
