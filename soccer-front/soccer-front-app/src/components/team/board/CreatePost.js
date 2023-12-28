a901ffc4a40fa1ef09f7a1acc6d2d712166f9b70
ljkhyeong <jolri24@gmail.com>
2023-12-28 22:19:54 +0900

    refactor : 모듈화로 인한 글 작성 페이지 기존 코드 수정

--- soccer-front/soccer-front-app/src/components/team/board/CreatePost.js
+++ soccer-front/soccer-front-app/src/components/team/board/CreatePost.js
@@ -1,18 +1,21 @@
 import {useState} from "react";
 import {Container, Form, Button} from "react-bootstrap";
 import {axiosInstance} from "../../../service/ApiService";
+import {handleInputChange, handleKeyDown} from "../../../service/CommonService";
 
-const PostPage = (props) => {
+const CreatePost = (props) => {
 
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
+    const [postForm, setPostForm] = useState({
+        title:'',
+        content:''
+    })
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        axiosInstance.post('/post', {
-            title:title,
-            content:content
-        }).then(response => {
+        axiosInstance.post('/post', postForm)
+            .then(response => {
             console.log(response);
             alert("게시글 작성 성공");
             props.setShowContainer('board');
@@ -30,8 +33,9 @@ const PostPage = (props) => {
                         type="text"
                         placeholder="제목을 입력하세요"
                         value={title}
-                        onChange={(e) => setTitle(e.target.value)}
+                        onChange={(e) => handleInputChange(e,postForm,setPostForm)}
                         required
+                        onKeyDown={(e) => handleKeyDown(e,handleSubmit)}
                     />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formContent">
@@ -41,8 +45,9 @@ const PostPage = (props) => {
                         rows={10}
                         placeholder="내용을 입력하세요"
                         value={content}
-                        onChange={(e) => setContent(e.target.value)}
+                        onChange={(e) => handleInputChange(e,postForm,setPostForm)}
                         required
+                        onKeyDown={(e) => handleKeyDown(e,handleSubmit)}
                     />
                 </Form.Group>
                 <Button onClick={handleSubmit}>작성</Button>
@@ -51,4 +56,4 @@ const PostPage = (props) => {
     );
 };
 
-export default PostPage;
+export default CreatePost;
