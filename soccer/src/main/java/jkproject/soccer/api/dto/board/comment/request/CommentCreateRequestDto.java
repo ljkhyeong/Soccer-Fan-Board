9c593afcfb4a4809eecd0351dc168c0ce74a8414
ljkhyeong <jolri24@gmail.com>
2023-12-28 19:53:02 +0900

    refactor: comment 요청 dto에 NotBlank valid 추가

--- soccer/src/main/java/jkproject/soccer/api/dto/board/comment/request/CommentCreateRequestDto.java
+++ soccer/src/main/java/jkproject/soccer/api/dto/board/comment/request/CommentCreateRequestDto.java
@@ -1,12 +1,16 @@
 package jkproject.soccer.api.dto.board.comment.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jkproject.soccer.domain.entity.board.comment.Comment;
 import jkproject.soccer.domain.entity.board.post.Post;
 import jkproject.soccer.domain.entity.user.User;
+import jkproject.soccer.web.common.exception.enums.ValidationMessage;
 import lombok.Data;
 
 @Data
 public class CommentCreateRequestDto {
+	
+	@NotBlank(message = ValidationMessage.Messages.NOT_NULL)
 	private String comment;
 
 	public Comment toEntity(User user, Post post) {
