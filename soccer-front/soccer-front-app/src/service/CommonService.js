be327d8cd244365f0ed6fb395e0ab93e780ab11c
ljkhyeong <jolri24@gmail.com>
2023-12-28 21:28:06 +0900

    feat : 프론트 공통되는 기능들 모듈화

---
+++ soccer-front/soccer-front-app/src/service/CommonService.js
@@ -0,0 +1,31 @@
+
+export const handleKeyDown = (event, callback) => {
+    if (event.key === 'Enter') {
+        if (event.nativeEvent.isComposing === false) {
+            event.preventDefault();
+            callback();
+        }
+    }
+}
+
+export const handleInputChange = (e, data, setData) => {
+    const {name, value} = e.target;
+    setData({
+        ...data,
+        [name] : value
+    });
+};
+
+export const initStateObject = (state) => {
+    const resetState = {};
+    for (const key in state) {
+        resetState[key] = "";
+    }
+    return initStateObject;
+};
+
+export const resetStates = (...setters) => {
+    setters.forEach(setter => {
+        setter('');
+    });
+};
