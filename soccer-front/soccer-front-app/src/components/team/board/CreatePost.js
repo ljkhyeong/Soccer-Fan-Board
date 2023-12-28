2d553fed9b2e25c9efd35f0cd6b12bebb99a9ed0
ljkhyeong <jolri24@gmail.com>
2023-12-29 03:04:01 +0900

    refactor: 게시글 작성 valid 에러 추가

--- soccer-front/soccer-front-app/src/components/team/board/CreatePost.js
+++ soccer-front/soccer-front-app/src/components/team/board/CreatePost.js
@@ -5,11 +5,13 @@ import {handleInputChange, handleKeyDown} from "../../../service/CommonService";
 
 const CreatePost = (props) => {
 
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
     const [postForm, setPostForm] = useState({
         title:'',
         content:''
+    })
+    const [errors, setErrors] = useState({
+        titleError:'',
+        contentError:''
     })
 
     const handleSubmit = (e) => {
@@ -21,6 +23,11 @@ const CreatePost = (props) => {
             props.setShowContainer('board');
         }).catch(error => {
             console.log(error);
+            const errorResult = error.response.data.result;
+            setErrors({
+                titleError: errorResult.valid_title,
+                contentError: errorResult.valid_content
+            })
         })
     };
 
@@ -30,25 +37,27 @@ const CreatePost = (props) => {
                 <Form.Group className="mb-3" controlId="formTitle">
                     <Form.Label>제목</Form.Label>
                     <Form.Control
+                        name="title"
                         type="text"
                         placeholder="제목을 입력하세요"
-                        value={title}
                         onChange={(e) => handleInputChange(e,postForm,setPostForm)}
                         required
                         onKeyDown={(e) => handleKeyDown(e,handleSubmit)}
                     />
+                    { errors.titleError && <Form.Text className="valid-error">{errors.titleError}</Form.Text>}
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formContent">
                     <Form.Label>내용</Form.Label>
                     <Form.Control
+                        name="content"
                         as="textarea"
                         rows={10}
                         placeholder="내용을 입력하세요"
-                        value={content}
                         onChange={(e) => handleInputChange(e,postForm,setPostForm)}
                         required
                         onKeyDown={(e) => handleKeyDown(e,handleSubmit)}
                     />
+                    { errors.contentError && <Form.Text className="valid-error">{errors.contentError}</Form.Text>}
                 </Form.Group>
                 <Button onClick={handleSubmit}>작성</Button>
             </Form>
