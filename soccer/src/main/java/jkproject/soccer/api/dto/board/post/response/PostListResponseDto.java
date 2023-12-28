299f5bffdd2786a1b616cb7502f9afb43f91d291
ljkhyeong <jolri24@gmail.com>
2023-12-28 19:52:25 +0900

    refactor: 게시글 리스트에 필요한 요소 dto에 더 추가

--- soccer/src/main/java/jkproject/soccer/api/dto/board/post/response/PostListResponseDto.java
+++ soccer/src/main/java/jkproject/soccer/api/dto/board/post/response/PostListResponseDto.java
@@ -1,5 +1,7 @@
 package jkproject.soccer.api.dto.board.post.response;
 
+import java.time.LocalDateTime;
+
 import jkproject.soccer.domain.entity.board.post.Post;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,11 +11,16 @@ import lombok.Getter;
 public class PostListResponseDto {
 	private Long postId;
 	private String title;
+	private String writer;
+	private LocalDateTime createdAt;
 
 	public static PostListResponseDto from(Post post) {
 		return PostListResponseDto.builder()
 			.postId(post.getPostId())
 			.title(post.getTitle())
+			.writer(post.getWriter())
+			.createdAt(post.getCreatedAt())
 			.build();
 	}
+	
 }
