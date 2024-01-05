583308d218bc4bdd15dd81287cc1e9736bca9c0d
ljkhyeong <jolri24@gmail.com>
2024-01-05 21:47:50 +0900

    feat: 위키 관련 컨트롤러, 서비스 로직 추가
    위키 최신문서 읽기와 위키문서 생성 기능 추가

--- soccer/src/main/java/jkproject/soccer/api/controller/wiki/WikiApiController.java
+++ soccer/src/main/java/jkproject/soccer/api/controller/wiki/WikiApiController.java
@@ -1,12 +1,40 @@
 package jkproject.soccer.api.controller.wiki;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
+import jkproject.soccer.api.common.dto.response.Response;
+import jkproject.soccer.api.dto.user.UserAuthenticationDto;
+import jkproject.soccer.api.dto.wiki.request.WikiDocCreateRequestDto;
+import jkproject.soccer.api.dto.wiki.response.WikiDocDetailResponseDto;
+import jkproject.soccer.domain.service.wiki.WikiService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("api/v1")
 @RequiredArgsConstructor
 public class WikiApiController {
+
+	private final WikiService wikiService;
+
+	@GetMapping("/{teamId}/wiki")
+	public Response<WikiDocDetailResponseDto> readNewWikiDoc(@PathVariable Long teamId) {
+		WikiDocDetailResponseDto wikiDocDto = wikiService.getNewWikiDocDetail(teamId);
+		return Response.success(wikiDocDto);
+	}
+
+	@PostMapping("/{teamId}/wiki/{wikiDocId}")
+	public Response<Void> createNewWikiDoc(@PathVariable Long teamId, @PathVariable Long wikiDocId,
+		@RequestBody @Valid WikiDocCreateRequestDto requestDto,
+		Errors errors, @AuthenticationPrincipal UserAuthenticationDto userDto) {
+		wikiService.createNewWikiDoc(wikiDocId, requestDto, userDto, errors);
+		return Response.success();
+	}
 }
--- soccer/src/main/java/jkproject/soccer/domain/service/wiki/WikiService.java
+++ soccer/src/main/java/jkproject/soccer/domain/service/wiki/WikiService.java
@@ -1,4 +1,59 @@
 package jkproject.soccer.domain.service.wiki;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.Errors;
+
+import jkproject.soccer.api.dto.user.UserAuthenticationDto;
+import jkproject.soccer.api.dto.wiki.request.WikiDocCreateRequestDto;
+import jkproject.soccer.api.dto.wiki.response.WikiDocDetailResponseDto;
+import jkproject.soccer.domain.entity.user.User;
+import jkproject.soccer.domain.entity.wiki.DocVersion;
+import jkproject.soccer.domain.entity.wiki.WikiDoc;
+import jkproject.soccer.domain.repository.team.TeamRepository;
+import jkproject.soccer.domain.repository.user.UserRepository;
+import jkproject.soccer.domain.repository.wiki.DocVersionRepository;
+import jkproject.soccer.domain.repository.wiki.WikiDocRepository;
+import jkproject.soccer.web.common.exception.ApplicationException;
+import jkproject.soccer.web.common.exception.enums.ErrorCode;
+import jkproject.soccer.web.common.validator.ValidationResultHandler;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class WikiService {
+
+	private final WikiDocRepository wikiDocRepository;
+	private final DocVersionRepository docVersionRepository;
+	private final TeamRepository teamRepository;
+	private final UserRepository userRepository;
+	private final ValidationResultHandler validationResultHandler;
+
+	@Transactional(readOnly = true)
+	public WikiDocDetailResponseDto getNewWikiDocDetail(Long teamId) {
+		WikiDoc wikiDoc = wikiDocRepository.findByTeamId(teamId)
+			.orElseThrow(() -> new ApplicationException(ErrorCode.NON_EXISTENT_WIKIDOC_ID));
+
+		DocVersion docVersion = docVersionRepository.findTopDocByWikiDoc(wikiDoc)
+			.orElseThrow(() -> new ApplicationException(ErrorCode.NON_EXISTENT_WIKIDOC_VERSION));
+
+		return WikiDocDetailResponseDto.from(wikiDoc, docVersion);
+	}
+
+	public void createNewWikiDoc(Long wikiDocId, WikiDocCreateRequestDto requestDto,
+		UserAuthenticationDto userDto, Errors errors) {
+
+		validationResultHandler.ifErrorsThrow(errors, ErrorCode.INVALID_LOGIN);
+
+		WikiDoc wikiDoc = wikiDocRepository.findById(wikiDocId)
+			.orElseThrow(() -> new ApplicationException(ErrorCode.NON_EXISTENT_WIKIDOC_ID));
+		User user = userRepository.findByLoginId(userDto.getLoginId())
+			.orElseThrow(() -> new ApplicationException(ErrorCode.NON_EXISTENT_USER_ID));
+		Integer topVersion = docVersionRepository.findTopVersionByWikiDoc(wikiDoc)
+			.orElseThrow(() -> new ApplicationException(ErrorCode.NON_EXISTENT_WIKIDOC_VERSION));
+
+		DocVersion newDoc = requestDto.toEntity(wikiDoc, topVersion, user);
+		docVersionRepository.save(newDoc);
+	}
 }
