17fda938fbac987076a7967c4e75678f1e60bc14
ljkhyeong <jolri24@gmail.com>
2024-01-05 21:48:05 +0900

    feat: 추가된 위키기능에 대한 Repository 메소드 추가

--- soccer/src/main/java/jkproject/soccer/domain/repository/wiki/DocVersionRepository.java
+++ soccer/src/main/java/jkproject/soccer/domain/repository/wiki/DocVersionRepository.java
@@ -1,8 +1,18 @@
 package jkproject.soccer.domain.repository.wiki;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import jkproject.soccer.domain.entity.wiki.DocVersion;
+import jkproject.soccer.domain.entity.wiki.WikiDoc;
 
 public interface DocVersionRepository extends JpaRepository<DocVersion, Long> {
+	@Query("SELECT dv FROM DocVersion dv WHERE dv.wikiDoc = :wikiDoc "
+		+ "AND dv.version = (SELECT MAX(dv2.version) FROM DocVersion dv2 WHERE dv2.wikiDoc = :wikiDoc)")
+	Optional<DocVersion> findTopDocByWikiDoc(WikiDoc wikiDoc);
+
+	@Query("SELECT MAX(dv.version) FROM DocVersion dv WHERE dv.wikiDoc = :wikiDoc")
+	Optional<Integer> findTopVersionByWikiDoc(WikiDoc wikiDoc);
 }
--- soccer/src/main/java/jkproject/soccer/domain/repository/wiki/WikiDocRepository.java
+++ soccer/src/main/java/jkproject/soccer/domain/repository/wiki/WikiDocRepository.java
@@ -1,8 +1,11 @@
 package jkproject.soccer.domain.repository.wiki;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import jkproject.soccer.domain.entity.wiki.WikiDoc;
 
 public interface WikiDocRepository extends JpaRepository<WikiDoc, Long> {
+	Optional<WikiDoc> findByTeamId(Long teamId);
 }
