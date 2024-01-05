be036d7248fd498cc2c4d2e878b10cd07f2a6811
ljkhyeong <jolri24@gmail.com>
2024-01-05 21:49:36 +0900

    feat: 추가된 위키기능을 위한 ErrorCode 추가

--- soccer/src/main/java/jkproject/soccer/web/common/exception/enums/ErrorCode.java
+++ soccer/src/main/java/jkproject/soccer/web/common/exception/enums/ErrorCode.java
@@ -9,10 +9,14 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
+	NON_EXISTENT_TEAM_ID(HttpStatus.BAD_REQUEST, "존재하지 않는 팀"),
 	NON_EXISTENT_USER_ID(HttpStatus.BAD_REQUEST, "존재하지 않는 회원"),
 	NON_EXISTENT_POST_ID(HttpStatus.BAD_REQUEST, "존재하지 않는 게시물"),
+	NON_EXISTENT_WIKIDOC_ID(HttpStatus.BAD_REQUEST, "존재하지 않는 위키문서"),
+	NON_EXISTENT_WIKIDOC_VERSION(HttpStatus.BAD_REQUEST, "해당하는 위키에 어떤 버전의 문서도 존재하지 않습니다."),
 	INVALID_LOGIN(HttpStatus.BAD_REQUEST, "유효하지 않은 로그인 시도"),
 	INVALID_JOIN(HttpStatus.BAD_REQUEST, "유효하지 않은 회원가입 시도"),
+	INVALID_CREATE_WIKI(HttpStatus.BAD_REQUEST, "유효하지 않은 위키문서 작성"),
 	INVALID_CREATE_POST(HttpStatus.BAD_REQUEST, "유효하지 않은 게시물 작성"),
 	INVALID_CREATE_COMMENT(HttpStatus.BAD_REQUEST, "유효하지 않은 댓글 작성"),
 	INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "유효하지 않은 비밀번호"),
