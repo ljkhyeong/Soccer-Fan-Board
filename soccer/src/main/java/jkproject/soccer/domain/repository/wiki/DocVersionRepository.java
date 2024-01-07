d949ec5c80dee8ad56f688442849ebe83ad16f25
ljkhyeong <jolri24@gmail.com>
2024-01-08 01:02:45 +0900

    feat: DocVersion 리포지토리 메소드 추가
    추가된 서비스 로직들에 해당하는 메소드

--- soccer/src/main/java/jkproject/soccer/domain/repository/wiki/DocVersionRepository.java
+++ soccer/src/main/java/jkproject/soccer/domain/repository/wiki/DocVersionRepository.java
@@ -2,6 +2,8 @@ package jkproject.soccer.domain.repository.wiki;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -15,4 +17,9 @@ public interface DocVersionRepository extends JpaRepository<DocVersion, Long> {
 
 	@Query("SELECT MAX(dv.version) FROM DocVersion dv WHERE dv.wikiDoc = :wikiDoc")
 	Optional<Integer> findTopVersionByWikiDoc(WikiDoc wikiDoc);
+
+	Optional<DocVersion> findByWikiDocAndVersion(WikiDoc wikiDoc, Integer version);
+
+	@Query("SELECT dv FROM DocVersion dv WHERE dv.wikiDoc.wikiDocId = :wikiDocId")
+	Page<DocVersion> findAllByWikiDocId(Long wikiDocId, Pageable pageable);
 }
