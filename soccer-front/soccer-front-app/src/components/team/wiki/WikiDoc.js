98b3a312d36af3a6b59f6fc96a1c30b46bc52916
ljkhyeong <jolri24@gmail.com>
2024-01-08 22:30:13 +0900

    refactor: 역사 페이지에서 해당하는 위키문서로 이동하도록 수정
    wikiDoc.js

--- soccer-front/soccer-front-app/src/components/team/wiki/WikiDoc.js
+++ soccer-front/soccer-front-app/src/components/team/wiki/WikiDoc.js
@@ -5,28 +5,50 @@ import {useNavigate, useParams} from "react-router-dom";
 
 
 const WikiDoc = () => {
-    const {teamName} = useParams();
+    const {teamCode, wikiDocId, version} = useParams();
     const [wikiDoc, setWikiDoc] = useState(null);
     const [error, setError] = useState('');
     const navigate = useNavigate();
 
     useEffect(() => {
-        axiosInstance.get(`/${teamName}/wiki`)
+        if (wikiDocId && version) {
+            showDoc(wikiDocId, version)
+        } else {
+            showFirstDoc();
+        }
+    }, [teamCode]);
+
+    const showFirstDoc = () => {
+        axiosInstance.get(`/${teamCode}/wiki`)
             .then(response => {
                 console.log(response);
                 setWikiDoc(response.data.result);
             }).catch(error => {
-                console.log(error);
-                setError(error.response.data.result);
+            console.log(error);
+            setError(error.response.data.result);
         })
-    }, [teamName]);
+    }
+
+    const showDoc = (wikiDocId, version) => {
+        axiosInstance.get(`/${teamCode}/wiki/${wikiDocId}`, {
+            params : {
+                version : version
+            }}).then(response => {
+            console.log(response);
+            setWikiDoc(response.data.result);
+        }).catch(error => {
+            console.log(error);
+        })
+    }
 
     return (
         <>
             {wikiDoc && <><h1>{wikiDoc.title}</h1>
                 <p>{wikiDoc.body}</p></>}
             {error && <p>{error}</p>}
-            <Button onClick={() => navigate('./create', {state: { body : wikiDoc?.body}})}>새 문서 만들기</Button>
+            {!wikiDocId && <Button onClick={() => navigate('../wiki/create', {state: {wikiDoc}})}>편집</Button>}
+            {wikiDoc ? <Button onClick={() => navigate('../wiki/history', {state: {wikiDoc}})}>역사</Button>
+                : <Button onClick={() => navigate('./create', {state: {wikiDoc}})}>새 문서 만들기</Button>}
         </>
     )
 }
