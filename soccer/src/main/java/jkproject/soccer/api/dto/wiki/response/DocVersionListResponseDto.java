e042d2c3e54607ff088ce35e85e51b374b7cfd8e
ljkhyeong <jolri24@gmail.com>
2024-01-14 06:09:12 +0900

    refactor: docversionListResponseDto writer 필드 추가

--- soccer/src/main/java/jkproject/soccer/api/dto/wiki/response/DocVersionListResponseDto.java
+++ soccer/src/main/java/jkproject/soccer/api/dto/wiki/response/DocVersionListResponseDto.java
@@ -10,12 +10,14 @@ import lombok.Getter;
 @Builder
 public class DocVersionListResponseDto {
 	private Long docVersionId;
+	private String writer;
 	private Integer version;
 	private LocalDateTime createdAt;
 
 	public static DocVersionListResponseDto from(DocVersion docVersion) {
 		return DocVersionListResponseDto.builder()
 			.docVersionId(docVersion.getDocVersionId())
+			.writer(docVersion.getWriter())
 			.version(docVersion.getVersion())
 			.createdAt(docVersion.getCreatedAt())
 			.build();
