82ac689c49385e4b12bab124632a17b5d5f9446d
ljkhyeong <jolri24@gmail.com>
2024-01-14 06:08:04 +0900

    refactor: AutoScrapService 엔티티 저장로직 추가
    완성 -> 메소드 분할 필요.

--- soccer/src/main/java/jkproject/soccer/web/common/crawler/service/AutoScrapService.java
+++ soccer/src/main/java/jkproject/soccer/web/common/crawler/service/AutoScrapService.java
@@ -14,7 +14,12 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Service;
 
+import jkproject.soccer.domain.entity.team.Team;
+import jkproject.soccer.domain.entity.team.player.Player;
 import jkproject.soccer.domain.repository.team.PlayerRepository;
+import jkproject.soccer.domain.repository.team.TeamRepository;
+import jkproject.soccer.web.common.exception.ApplicationException;
+import jkproject.soccer.web.common.exception.enums.ErrorCode;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -22,13 +27,17 @@ import lombok.RequiredArgsConstructor;
 public class AutoScrapService {
 
 	private final PlayerRepository playerRepository;
+	private final TeamRepository teamRepository;
 
+	// TODO 메소드 분할 필요
 	public void scrapPlayersInfo() {
 		System.setProperty("webdriver.chrome.driver", "/opt/homebrew/bin/chromedriver");
 		WebDriver driver = new ChromeDriver();
 
-		driver.get("https://www.premierleague.com/players");
+		// 특정 조건이 충족될 때까지 기다리고 NoSuchException을 던진다.
+		// driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
+		driver.get("https://www.premierleague.com/players");
 		// 특정 조건이 충족될 때까지 기다리고 TimeoutExcetpion을 던진다.
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
@@ -38,7 +47,7 @@ public class AutoScrapService {
 		WebElement closeAdvertButton = driver.findElement(By.id("advertClose"));
 		closeAdvertButton.click();
 
-		// 클럽 설정
+		// 렌더링할 클럽 설정
 		WebElement clubsButton = wait.until(ExpectedConditions.visibilityOfElementLocated(
 			By.cssSelector(
 				"div.dropDown.mobile[data-listen-keypress='true'] div.current[data-dropdown-current='clubs']")));
@@ -51,7 +60,8 @@ public class AutoScrapService {
 
 		for (WebElement clubOption : clubOptions) {
 			String teamName = clubOption.getAttribute("data-option-name");
-			System.out.println(teamName);
+			Team team = teamRepository.findByName(teamName)
+				.orElseThrow(() -> new ApplicationException(ErrorCode.NON_EXISTENT_TEAM_NAME));
 			if (teamName.equals("All Clubs")) {
 				continue;
 			}
@@ -64,21 +74,34 @@ public class AutoScrapService {
 			} catch (ElementClickInterceptedException e) {
 			}
 
-			// 홈페이지 렌더링 기다리기
+			// 선수상세 페이지 렌더링 기다리기
 			try {
 				wait.until(ExpectedConditions.visibilityOfElementLocated(
 					By.cssSelector("tbody.dataContainer.indexSection div.loader")));
 			} catch (TimeoutException e) {
-				System.out.println("gogo");
 			}
+			wait.until(
+				ExpectedConditions.presenceOfElementLocated(By.cssSelector("span.player__nationality")));
 
-			List<WebElement> elementList = wait.until(
-				ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("span.player__nationality")));
-			List<WebElement> elementList2 = wait.until(
-				ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("tr.player td a.player__name")));
+			// 선수들 정보 스크래핑
+			List<WebElement> playerInfoList = driver.findElements(By.cssSelector("tr.player"));
 
-			for (WebElement element : elementList2) {
-				System.out.print(element.getText());
+			for (WebElement element : playerInfoList) {
+				WebElement ImgAndName = element.findElement(By.className("player__name"));
+				String image = ImgAndName.getAttribute("href");
+				String name = ImgAndName.getText();
+				String position = element.findElement(By.className("player__position")).getText();
+				String country = element.findElement(By.className("player__country")).getText();
+
+				Player player = Player.builder()
+					.imagePath(image)
+					.name(name)
+					.position(position)
+					.country(country)
+					.team(team)
+					.build();
+
+				playerRepository.save(player);
 			}
 
 			clubsButton = wait.until(ExpectedConditions.visibilityOfElementLocated(
@@ -92,6 +115,7 @@ public class AutoScrapService {
 		}
 
 		driver.quit();
+
 	}
 
 	private static void scrollToElement(WebDriver driver, WebElement container, WebElement element) {
