9ef5eac4cbed9f7e4ee8d29808f044d3a75bd3f9
ljkhyeong <jolri24@gmail.com>
2024-01-15 22:19:07 +0900

    refactor: AutoScrapService 메소드 분할

--- soccer/src/main/java/jkproject/soccer/web/common/crawler/service/AutoScrapService.java
+++ soccer/src/main/java/jkproject/soccer/web/common/crawler/service/AutoScrapService.java
@@ -4,7 +4,6 @@ import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.ElementClickInterceptedException;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
@@ -29,96 +28,91 @@ public class AutoScrapService {
 	private final PlayerRepository playerRepository;
 	private final TeamRepository teamRepository;
 
-	// TODO 메소드 분할 필요
 	public void scrapPlayersInfo() {
 		System.setProperty("webdriver.chrome.driver", "/opt/homebrew/bin/chromedriver");
 		WebDriver driver = new ChromeDriver();
-
-		// 특정 조건이 충족될 때까지 기다리고 NoSuchException을 던진다.
-		// driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
 		driver.get("https://www.premierleague.com/players");
-		// 특정 조건이 충족될 때까지 기다리고 TimeoutExcetpion을 던진다.
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-		// 쿠키 허용, 광고 닫기
-		WebElement acceptCookieButton = driver.findElement(By.id("onetrust-accept-btn-handler"));
-		acceptCookieButton.click();
-		WebElement closeAdvertButton = driver.findElement(By.id("advertClose"));
-		closeAdvertButton.click();
+		acceptCookieAndCloseAdvert(driver);
 
-		// 렌더링할 클럽 설정
 		WebElement clubsButton = wait.until(ExpectedConditions.visibilityOfElementLocated(
 			By.cssSelector(
 				"div.dropDown.mobile[data-listen-keypress='true'] div.current[data-dropdown-current='clubs']")));
-		clubsButton.click();
-		wait.until(ExpectedConditions.visibilityOfElementLocated(
-			By.cssSelector("div.dropDown.mobile.open")));
+		openClubListAndWait(clubsButton, wait);
+
+		// 열린 클럽 리스트 ul, li 가져오기
 		WebElement clubContainer = driver.findElement(By.cssSelector("ul.dropdownList[data-dropdown-list='clubs']"));
 		List<WebElement> clubOptions = driver.findElements(
 			By.cssSelector("ul.dropdownList[data-dropdown-list='clubs'] li"));
 
 		for (WebElement clubOption : clubOptions) {
 			String teamName = clubOption.getAttribute("data-option-name");
-			Team team = teamRepository.findByName(teamName)
-				.orElseThrow(() -> new ApplicationException(ErrorCode.NON_EXISTENT_TEAM_NAME));
 			if (teamName.equals("All Clubs")) {
 				continue;
 			}
+			Team team = teamRepository.findByName(teamName)
+				.orElseThrow(() -> new ApplicationException(ErrorCode.NON_EXISTENT_TEAM_NAME));
 
-			// 해당 클럽을 찾도록 ul 태그 내에서 스크롤
-			scrollToElement(driver, clubContainer, clubOption);
+			scrollToElementAndClick(driver, clubContainer, clubOption);
+			waitPlayerInfo(wait);
+			playerInfoToEntityAndSave(driver, team);
 
-			try {
-				clubOption.click();
-			} catch (ElementClickInterceptedException e) {
-			}
-
-			// 선수상세 페이지 렌더링 기다리기
-			try {
-				wait.until(ExpectedConditions.visibilityOfElementLocated(
-					By.cssSelector("tbody.dataContainer.indexSection div.loader")));
-			} catch (TimeoutException e) {
-			}
-			wait.until(
-				ExpectedConditions.presenceOfElementLocated(By.cssSelector("span.player__nationality")));
-
-			// 선수들 정보 스크래핑
-			List<WebElement> playerInfoList = driver.findElements(By.cssSelector("tr.player"));
-
-			for (WebElement element : playerInfoList) {
-				WebElement ImgAndName = element.findElement(By.className("player__name"));
-				String image = ImgAndName.getAttribute("href");
-				String name = ImgAndName.getText();
-				String position = element.findElement(By.className("player__position")).getText();
-				String country = element.findElement(By.className("player__country")).getText();
-
-				Player player = Player.builder()
-					.imagePath(image)
-					.name(name)
-					.position(position)
-					.country(country)
-					.team(team)
-					.build();
-
-				playerRepository.save(player);
-			}
-
-			clubsButton = wait.until(ExpectedConditions.visibilityOfElementLocated(
-				By.cssSelector(
-					"div.dropDown.mobile[data-listen-keypress='true'] div.current[data-dropdown-current='clubs']")));
-			clubsButton.click();
-
-			// 리스트가 다시 로드될 때까지 기다림
-			wait.until(ExpectedConditions.visibilityOfElementLocated(
-				By.cssSelector("div.dropDown.mobile.open")));
+			openClubListAndWait(clubsButton, wait);
 		}
 
 		driver.quit();
 
 	}
 
-	private static void scrollToElement(WebDriver driver, WebElement container, WebElement element) {
+	private static void openClubListAndWait(WebElement clubsButton, WebDriverWait wait) {
+		clubsButton.click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(
+			By.cssSelector("div.dropDown.mobile.open")));
+	}
+
+	private void playerInfoToEntityAndSave(WebDriver driver, Team team) {
+		List<WebElement> playerInfoList = driver.findElements(By.cssSelector("tr.player"));
+
+		for (WebElement playerOption : playerInfoList) {
+			WebElement nameAndInfo = playerOption.findElement(By.className("player__name"));
+			String info = nameAndInfo.getAttribute("href");
+			String name = nameAndInfo.getText();
+			String image = playerOption.findElement(By.cssSelector(".img.player__name-image")).getAttribute("src");
+			String position = playerOption.findElement(By.className("player__position")).getText();
+			String country = playerOption.findElement(By.className("player__country")).getText();
+
+			Player player = Player.builder()
+				.infoLink(info)
+				.imagePath(image)
+				.name(name)
+				.position(position)
+				.country(country)
+				.team(team)
+				.build();
+
+			playerRepository.save(player);
+		}
+	}
+
+	private static void waitPlayerInfo(WebDriverWait wait) {
+		try {
+			wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.cssSelector("tbody.dataContainer.indexSection div.loader")));
+		} catch (TimeoutException e) {
+		}
+		wait.until(
+			ExpectedConditions.presenceOfElementLocated(By.cssSelector("span.player__nationality")));
+	}
+
+	private static void acceptCookieAndCloseAdvert(WebDriver driver) {
+		WebElement acceptCookieButton = driver.findElement(By.id("onetrust-accept-btn-handler"));
+		acceptCookieButton.click();
+		// WebElement closeAdvertButton = driver.findElement(By.id("advertClose"));
+		// closeAdvertButton.click();
+	}
+
+	private static void scrollToElementAndClick(WebDriver driver, WebElement container, WebElement element) {
 		JavascriptExecutor js = (JavascriptExecutor)driver;
 		js.executeScript(
 			"arguments[0].scrollTop = arguments[1].offsetTop - arguments[0].offsetTop;",
@@ -126,6 +120,7 @@ public class AutoScrapService {
 		);
 		try {
 			Thread.sleep(500); // 컨테이너 스크롤 후 DOM 업데이트 대기 시간
+			element.click();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
