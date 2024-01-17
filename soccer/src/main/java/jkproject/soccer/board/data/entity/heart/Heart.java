e36566204e1b1a56bc278709c98ec4452f6fabc4
ljkhyeong <jolri24@gmail.com>
2024-01-17 22:32:31 +0900

    feat: 좋아요 엔티티 추가

---
+++ soccer/src/main/java/jkproject/soccer/board/data/entity/heart/Heart.java
@@ -0,0 +1,41 @@
+package jkproject.soccer.board.data.entity.heart;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jkproject.soccer.board.data.entity.post.Post;
+import jkproject.soccer.user.data.entity.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "HEART_TB")
+public class Heart {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "heart_id")
+	private Long heartId;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@ManyToOne
+	@JoinColumn(name = "post_id")
+	private Post post;
+
+	@Builder
+	public Heart(User user, Post post) {
+		this.user = user;
+		this.post = post;
+	}
+}
