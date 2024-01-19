dd1b72b30cc50acb75ef77863603187e75eef6b9
ljkhyeong <jolri24@gmail.com>
2024-01-19 20:04:03 +0900

    improve: 게시글 상세 프론트 개선
    좋아요 기능, 관련 로직 추가

--- soccer-front/soccer-front-app/src/components/team/board/PostDetail.js
+++ soccer-front/soccer-front-app/src/components/team/board/PostDetail.js
@@ -1,5 +1,5 @@
 import {useEffect, useRef, useState} from "react";
-import {Container, Row, Col, Card} from "react-bootstrap";
+import {Container, Row, Col, Card, Button} from "react-bootstrap";
 import {axiosInstance, formatDateTime} from "../../../service/ApiService";
 import Comments from "./Comments";
 import {useParams} from "react-router-dom";
@@ -14,13 +14,27 @@ const PostDetail = () => {
     }, []);
 
     const renderPostDetail = () => {
-        axiosInstance.get(`${teamCode}/posts/${postId}`)
-            .then(response => {
-                console.log(response)
-                setPostDetail(response.data.result)
-            }).catch(error => {
+        axiosInstance.get(`${teamCode}/posts/${postId}`
+        ).then(response => {
+                console.log(response);
+                setPostDetail(response.data.result);
+        }).catch(error => {
             console.log(error);
         });
+    }
+
+    const handleLike = () => {
+        axiosInstance.post(`/${teamCode}/posts/${postId}/heart`
+        ).then(response => {
+                console.log(response);
+                setPostDetail(prevDetails => ({
+                    ...prevDetails,
+                    heartCount : prevDetails.heartCount+1
+                }))
+        }).catch(error => {
+            console.log(error);
+            alert(error.response.data.result);
+        })
     }
 
     return (
@@ -28,17 +42,25 @@ const PostDetail = () => {
           <Row className="justify-content-md-center mt-5">
               <Col md={8}>
                   <Card>
-                      <Card.Header as="h5">{postDetail.title}</Card.Header>
+                      <Card.Header as="h3">{postDetail.title}</Card.Header>
+                      <Card.Header>
+                          작성일: {formatDateTime(postDetail.createdAt)}
+                      </Card.Header>
                       <Card.Body>
-                          <Card.Title>작성자 : {postDetail.writer}</Card.Title>
+                      <Card.Title style={{ textAlign: 'right' }}>작성자 : {postDetail.writer}</Card.Title>
+                          <div style={{ textAlign: 'right' }}>조회수: {postDetail.viewCount} 좋아요: {postDetail.heartCount}</div>
                           <Card.Text>
                               {postDetail.content}
                           </Card.Text>
+                          <div style={{display: 'flex', justifyContent: 'center'}}>
+                          <Button onClick={handleLike}
+                              variant="outline-primary"
+                          >
+                              좋아요: {postDetail.heartCount}
+                          </Button>
+                          </div>
                       </Card.Body>
-                      <Card.Footer>
-                          작성일: {formatDateTime(postDetail.createdAt)}
-                          조회수: {postDetail.viewCount}
-                      </Card.Footer>
+
                   </Card>
                   <Comments />
               </Col>
