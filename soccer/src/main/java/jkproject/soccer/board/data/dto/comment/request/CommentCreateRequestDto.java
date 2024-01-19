cb178193657c9570222a3db2cb233f0c0ad85a66
ljkhyeong <jolri24@gmail.com>
2024-01-19 20:01:07 +0900

    improve: CommentCreateDTO 개선
    댓글인지 대댓글인지 확인하는 로직 추가

--- soccer/src/main/java/jkproject/soccer/board/data/dto/comment/request/CommentCreateRequestDto.java
+++ soccer/src/main/java/jkproject/soccer/board/data/dto/comment/request/CommentCreateRequestDto.java
@@ -3,22 +3,29 @@ package jkproject.soccer.board.data.dto.comment.request;
 import jakarta.validation.constraints.NotBlank;
 import jkproject.soccer.board.data.entity.comment.Comment;
 import jkproject.soccer.board.data.entity.post.Post;
-import jkproject.soccer.user.data.entity.User;
 import jkproject.soccer.common.exception.enums.ValidationMessage;
+import jkproject.soccer.user.data.entity.User;
 import lombok.Data;
 
 @Data
 public class CommentCreateRequestDto {
 
+	private Long parentId;
 	@NotBlank(message = ValidationMessage.Messages.NOT_NULL)
 	private String comment;
 
-	public Comment toEntity(User user, Post post) {
-		return Comment.builder()
+	public Comment toEntity(Comment parent, User user, Post post) {
+		Comment.CommentBuilder builder = Comment.builder()
 			.commenter(user.getNickname())
 			.comment(comment)
 			.user(user)
-			.post(post)
-			.build();
+			.post(post);
+
+		if (parent != null) {
+			builder.parent(parent);
+		}
+
+		return builder.build();
 	}
+
 }
