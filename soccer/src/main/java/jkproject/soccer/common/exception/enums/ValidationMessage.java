0e6c21123d938134444f41d42934d24ded3db68a
ljkhyeong <jolri24@gmail.com>
2024-01-20 14:25:15 +0900

    add: Validation 메세지 추가
    회원가입 패턴 검증 관련

--- soccer/src/main/java/jkproject/soccer/common/exception/enums/ValidationMessage.java
+++ soccer/src/main/java/jkproject/soccer/common/exception/enums/ValidationMessage.java
@@ -13,13 +13,18 @@ import lombok.Getter;
 public enum ValidationMessage {
 
 	CANNOT_BE_NULL(Messages.NOT_NULL),
-	UNMATCHED_LOGINID(Messages.LOGIN_REQUIREMENTS),
+	UNMATCHED_LOGINID(Messages.LOGINID_REQUIREMENTS),
 	EXISTS_LOGINID(Messages.EXISTS_LOGINID),
 	EXISTS_NICKNAME(Messages.EXISTS_NICKNAME);
 
 	public static class Messages {
 		public static final String NOT_NULL = "필수로 입력되어야 합니다.";
-		public static final String LOGIN_REQUIREMENTS = "아이디는 4자 이상, 15자 이하의 영문자 혹은 영문자와 숫자의 조합이어야 합니다.";
+		public static final String LOGINID_REQUIREMENTS = "아이디는 영문, 숫자를 포함한 6-20자리여야 합니다.";
+		public static final String NICKNAME_REQUIREMENTS = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.";
+		public static final String PASSWORD_REQUIREMENTS = "비밀번호는 공백을 제외하고 영문, 숫자, 특수문자를 포함한 8~16자리여야 합니다.";
+		public static final String EMAIL_REQUIREMENTS = "잘못된 이메일 형식입니다.";
+		public static final String PHONENUMBER_REQUIREMENTS = "잘못된 전화번호 형식입니다.";
+
 		public static final String EXISTS_LOGINID = "이미 존재하는 아이디입니다.";
 		public static final String EXISTS_NICKNAME = "이미 존재하는 닉네임입니다.";
 	}
