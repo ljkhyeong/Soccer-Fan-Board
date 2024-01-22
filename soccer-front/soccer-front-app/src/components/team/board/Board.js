c998c19b22f342e85e224880c8dc808f6be0dc78
ljkhyeong <jolri24@gmail.com>
2024-01-23 02:40:58 +0900

    improve: 게시판 리스트 프론트 개선
    베스트글목록, 전체목록 조회 분류

--- soccer-front/soccer-front-app/src/components/team/board/Board.js
+++ soccer-front/soccer-front-app/src/components/team/board/Board.js
@@ -1,4 +1,4 @@
-import {Button, Form, FormControl, Table} from "react-bootstrap";
+import {Button, Form, FormControl, Tab, Table, Tabs} from "react-bootstrap";
 import {useEffect, useState} from "react";
 import axios from "axios";
 import {axiosInstance, formatDateTime} from "../../../service/ApiService";
@@ -15,14 +15,44 @@ const Board = (props) => {
         type: 'title',
         keyword: ''
     })
+    const [activeTab, setActiveTab] = useState('allPosts');
     const navigate = useNavigate();
 
     useEffect(() => {
-        renderPostList(searchCondition);
-    }, [currentPage]);
+        handleRenderPosts();
+    }, [currentPage, activeTab]);
+
+
+    const handleRenderPosts = () => {
+        if (activeTab === 'allPosts') {
+            renderPostList(searchCondition);
+        }
+
+        if (activeTab === 'bestPosts') {
+            renderBestPostList(searchCondition)
+        }
+    }
 
     const renderPostList = (condition = {}) => {
         axiosInstance.get(`/${teamCode}/posts`, {
+            params : {
+                ...condition,
+                page: currentPage,
+                size: 10
+            }
+        })
+            .then(response => {
+                setPosts(response.data.result.content);
+                setTotalPage(response.data.result.totalPages);
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    const renderBestPostList = (condition = {}) => {
+        axiosInstance.get(`/${teamCode}/posts/best`, {
             params : {
                 ...condition,
                 page: currentPage,
@@ -49,7 +79,7 @@ const Board = (props) => {
 
     const handleSearch = () => {
         setCurrentPage(0);
-        renderPostList(searchCondition);
+        handleRenderPosts();
         console.log(searchCondition);
     }
 
@@ -62,37 +92,74 @@ const Board = (props) => {
     return (
         <>
             <Button onClick={handlePostForm}>글 작성</Button>
-            <Table className="mt-4" hover>
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>제목</th>
-                    <th>글쓴이</th>
-                    <th>작성일</th>
-                    <th>조회수</th>
-                    <th>좋아요</th>
-                </tr>
-                </thead>
-                <tbody>
-                {posts.map((post, index) => (
-                    <tr key={post.postId} onClick={() => handleShowPost(post.postId)} style={{cursor: "pointer"}}>
-                        <td>{index + 1}</td>
-                        <td dangerouslySetInnerHTML={{__html: highlightKeyword(post.title, searchCondition.keyword)}}></td>
-                        <td>{post.writer}</td>
-                        <td>{formatDateTime(post.createdAt)}</td>
-                        <td>{post.viewCount}</td>
-                        <td>{post.heartCount}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </Table>
-            <div>
-                <Button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 0}>이전</Button>
-                {Array.from({length: totalPage}, (_, index) => (
-                    <Button variant="light" key={index} onClick={() => setCurrentPage(index)}>{index + 1}</Button>
-                ))}
-                <Button onClick={() => setCurrentPage(currentPage + 1)} disabled={posts.length < 10}>다음</Button>
-            </div>
+            <Tabs activeKey={activeTab} onSelect={(k) => setActiveTab(k)} className="mb-3">
+                <Tab eventKey="allPosts" title="전체목록">
+                    <Table className="mt-4" hover>
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>제목</th>
+                            <th>글쓴이</th>
+                            <th>작성일</th>
+                            <th>조회수</th>
+                            <th>좋아요</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {posts.map((post, index) => (
+                            <tr key={post.postId} onClick={() => handleShowPost(post.postId)} style={{cursor: "pointer"}}>
+                                <td>{post.postId}</td>
+                                <td dangerouslySetInnerHTML={{__html: highlightKeyword(post.title, searchCondition.keyword)}}></td>
+                                <td>{post.writer}</td>
+                                <td>{formatDateTime(post.createdAt)}</td>
+                                <td>{post.viewCount}</td>
+                                <td>{post.heartCount}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </Table>
+                    <div>
+                        <Button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 0}>이전</Button>
+                        {Array.from({length: totalPage}, (_, index) => (
+                            <Button variant="light" key={index} onClick={() => setCurrentPage(index)}>{index + 1}</Button>
+                        ))}
+                        <Button onClick={() => setCurrentPage(currentPage + 1)} disabled={posts.length < 10}>다음</Button>
+                    </div>
+                </Tab>
+                <Tab eventKey="bestPosts" title="추천글">
+                    <Table className="mt-4" hover>
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>제목</th>
+                            <th>글쓴이</th>
+                            <th>작성일</th>
+                            <th>조회수</th>
+                            <th>좋아요</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {posts.map((post, index) => (
+                            <tr key={post.postId} onClick={() => handleShowPost(post.postId)} style={{cursor: "pointer"}}>
+                                <td>{post.postId}</td>
+                                <td dangerouslySetInnerHTML={{__html: highlightKeyword(post.title, searchCondition.keyword)}}></td>
+                                <td>{post.writer}</td>
+                                <td>{formatDateTime(post.createdAt)}</td>
+                                <td>{post.viewCount}</td>
+                                <td>{post.heartCount}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </Table>
+                    <div>
+                        <Button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 0}>이전</Button>
+                        {Array.from({length: totalPage}, (_, index) => (
+                            <Button variant="light" key={index} onClick={() => setCurrentPage(index)}>{index + 1}</Button>
+                        ))}
+                        <Button onClick={() => setCurrentPage(currentPage + 1)} disabled={posts.length < 10}>다음</Button>
+                    </div>
+                </Tab>
+            </Tabs>
             <div style={{ display: 'flex', marginTop: '10px' }}>
                 <Form.Select className="mr-2" name="type" style={{ width: 'auto', height: '38px' }}
                              onChange={(e) => handleInputChange(e, searchCondition, setSearchCondition)}>
