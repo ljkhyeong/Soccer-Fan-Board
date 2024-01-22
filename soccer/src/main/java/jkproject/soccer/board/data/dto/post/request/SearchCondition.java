d3402e3cd8449b45d5143b45ab485782e7bd9e5b
ljkhyeong <jolri24@gmail.com>
2024-01-22 23:38:45 +0900

    feat: 게시판 검색 requestDto 추가

---
+++ soccer/src/main/java/jkproject/soccer/board/data/dto/post/request/SearchCondition.java
@@ -0,0 +1,11 @@
+package jkproject.soccer.board.data.dto.post.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class SearchCondition {
+	private String type;
+	private String keyword;
+}
