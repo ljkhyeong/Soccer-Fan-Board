bcf40d6229b96780f61d360c0ab2074f7b2cca65
ljkhyeong <jolri24@gmail.com>
2024-01-24 00:32:14 +0900

    feat: 게시글 업데이트 dto 추가

--- soccer/src/main/java/jkproject/soccer/board/data/dto/post/request/PostUpdateRequestDto.java
+++ soccer/src/main/java/jkproject/soccer/board/data/dto/post/request/PostUpdateRequestDto.java
@@ -1,4 +1,14 @@
 package jkproject.soccer.board.data.dto.post.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jkproject.soccer.common.exception.enums.ValidationMessage;
+import lombok.Data;
+
+@Data
 public class PostUpdateRequestDto {
+	@NotBlank(message = ValidationMessage.Messages.NOT_NULL)
+	private String title;
+	@NotBlank(message = ValidationMessage.Messages.NOT_NULL)
+	private String content;
+
 }
