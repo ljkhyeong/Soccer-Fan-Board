06bda50f9396423a1b0103c44fb234c6b85a342c
ljkhyeong <jolri24@gmail.com>
2024-01-24 06:48:56 +0900

    feat: (프론트) 인증 관련 context API 추가
    로그인 상태, 로그인ID 전역으로 관리하기 위함

--- soccer-front/soccer-front-app/src/App.js
+++ soccer-front/soccer-front-app/src/App.js
@@ -3,17 +3,20 @@ import {BrowserRouter,Routes, Route} from 'react-router-dom';
 import Main from './components/main/Main';
 import Team from './components/main/Team';
 import './App.css';
+import {AuthProvider} from "./auth/AuthContext";
 
 
 const App = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Main/>}/>
-          <Route path='team/:teamCode/*' element={<Team/>}/>
-        </Routes>
-      </BrowserRouter>
+        <AuthProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<Main/>}/>
+              <Route path='team/:teamCode/*' element={<Team/>}/>
+            </Routes>
+          </BrowserRouter>
+        </AuthProvider>
     </Suspense>
   );
 }
---
+++ soccer-front/soccer-front-app/src/auth/AuthContext.js
@@ -0,0 +1,35 @@
+import {createContext, useContext, useEffect, useState} from "react";
+import {axiosInstance, getAccessToken} from "../service/ApiService";
+
+const AuthContext = createContext(null);
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({children}) => {
+    const [isLogin, setIsLogin] = useState(false);
+    const [loginId, setLoginId] = useState("");
+
+    useEffect(() => {
+        if (!getAccessToken()) {
+            axiosInstance.post('/auth/refresh', {})
+                .then(() => {
+                    if (getAccessToken()) {
+                        setIsLogin(true);
+                    } else {
+                        setIsLogin(false);
+                    }
+                })
+                .catch((e) => {
+                    console.log(e);
+                })
+        } else {
+            setIsLogin(true);
+        }
+    }, [isLogin]);
+
+    return (
+        <AuthContext.Provider value={{ isLogin, setIsLogin, loginId, setLoginId}}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
