5d403d2e085775f6c2c190ef897eb8cc4c81f19a
ljkhyeong <jolri24@gmail.com>
2024-01-24 06:45:50 +0900

    improve: PostCreateRequestDto 개선
    비회원일 시 임시닉네임 반환하도록 개선

--- soccer/src/main/java/jkproject/soccer/board/data/dto/post/request/PostCreateRequestDto.java
+++ soccer/src/main/java/jkproject/soccer/board/data/dto/post/request/PostCreateRequestDto.java
@@ -1,5 +1,7 @@
 package jkproject.soccer.board.data.dto.post.request;
 
+import org.springframework.lang.Nullable;
+
 import jakarta.validation.constraints.NotBlank;
 import jkproject.soccer.board.data.entity.post.Post;
 import jkproject.soccer.common.exception.enums.ValidationMessage;
@@ -9,18 +11,26 @@ import lombok.Data;
 
 @Data
 public class PostCreateRequestDto {
+
+	@NotBlank(message = ValidationMessage.Messages.NOT_NULL)
+	private String tempNickname;
 	@NotBlank(message = ValidationMessage.Messages.NOT_NULL)
 	private String title;
 	@NotBlank(message = ValidationMessage.Messages.NOT_NULL)
 	private String content;
 
-	public Post toEntity(Team team, User user) {
+	public Post toEntity(Team team, @Nullable User user, String ipAddress) {
+		// TODO 로컬테스트 환경에서는 IPv6 반환 -> IPv4로 코드 수정 필요
+		String writerNickname = (user != null) ? user.getNickname() :
+			(tempNickname + ("(" + ipAddress.split(":")[0] + "." + ipAddress.split(":")[1]) + ")");
 		return Post.builder()
 			.title(title)
 			.content(content)
-			.writer(user.getNickname())
+			.writer(writerNickname)
+			.ipAddress(ipAddress)
 			.team(team)
 			.user(user)
 			.build();
 	}
+
 }
