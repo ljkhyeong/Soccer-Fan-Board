2a0264eaa3737fdf2c71185b3ebfe9b4839021d2
ljkhyeong <jolri24@gmail.com>
2024-01-28 05:25:01 +0900

    Fix: 비밀번호 틀려도 로그인 되는 버그 해결
    잘못된 로직 수정

--- soccer/src/main/java/jkproject/soccer/auth/service/AuthService.java
+++ soccer/src/main/java/jkproject/soccer/auth/service/AuthService.java
@@ -5,17 +5,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.Errors;
 
+import jkproject.soccer.auth.config.jwt.JwtTokenProvider;
+import jkproject.soccer.auth.config.jwt.TokenType;
 import jkproject.soccer.auth.data.dto.request.LoginRequestDto;
 import jkproject.soccer.auth.data.dto.response.LoginResponseDto;
 import jkproject.soccer.auth.data.dto.response.RefreshResponseDto;
-import jkproject.soccer.user.repository.UserRepository;
-import jkproject.soccer.user.data.entity.User;
-import jkproject.soccer.auth.config.jwt.JwtTokenProvider;
-import jkproject.soccer.auth.config.jwt.TokenType;
 import jkproject.soccer.auth.repository.RefreshTokenRepository;
 import jkproject.soccer.common.exception.ApplicationException;
 import jkproject.soccer.common.exception.enums.ErrorCode;
 import jkproject.soccer.common.validator.ValidationResultHandler;
+import jkproject.soccer.user.data.entity.User;
+import jkproject.soccer.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -78,7 +78,7 @@ public class AuthService {
 		User user = userRepository.findByLoginId(requestDto.getLoginId())
 			.orElseThrow(() -> new ApplicationException(ErrorCode.NON_EXISTENT_USER_ID));
 
-		if (passwordEncoder.matches(user.getPassword(), requestDto.getPassword())) {
+		if (!passwordEncoder.matches(requestDto.getPassword(), user.getPassword())) {
 			throw new ApplicationException(ErrorCode.INVALID_PASSWORD);
 		}
 		return user;
