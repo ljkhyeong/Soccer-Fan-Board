8dfb5e3aca9176b6d90cad7cd637bb466538cf1f
ljkhyeong <jolri24@gmail.com>
2024-02-13 10:03:22 +0900

    Fix: 싫어요를 눌러도 좋아요 수가 증가하는 버그 해결
    @RequestBody이 없어 파라미터 dto 바인딩이 제대로 안됌

--- soccer/src/main/java/jkproject/soccer/board/controller/heart/HeartApiController.java
+++ soccer/src/main/java/jkproject/soccer/board/controller/heart/HeartApiController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,7 @@ public class HeartApiController {
 
 	@PostMapping("/{teamCode}/posts/{postId}/heart")
 	public Response<Void> createHeart(@PathVariable String teamCode, @PathVariable Long postId,
-		HeartCreateRequestDto requestDto,
+		@RequestBody HeartCreateRequestDto requestDto,
 		@AuthenticationPrincipal UserAuthenticationDto userDto) {
 		heartService.createHeart(postId, requestDto, userDto);
 		return Response.success();
