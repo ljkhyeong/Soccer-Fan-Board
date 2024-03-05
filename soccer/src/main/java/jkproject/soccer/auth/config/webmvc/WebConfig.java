6951e9512f463877163fad74b5baceb4c69a7ebc
ljkhyeong <jolri24@gmail.com>
2024-03-05 22:37:11 +0900

    Fix: 배포를 위한 cors 허용 url 수정
    추후 환경변수로 변경 예정

--- soccer/src/main/java/jkproject/soccer/auth/config/webmvc/WebConfig.java
+++ soccer/src/main/java/jkproject/soccer/auth/config/webmvc/WebConfig.java
@@ -10,7 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("https://localhost:3000")
+			.allowedOrigins("http://13.125.252.116", "https://13.125.252.116",
+				"http://ec2-13-125-252-116.ap-northeast-2.compute.amazonaws.com/",
+				"https://ec2-13-125-252-116.ap-northeast-2.compute.amazonaws.com/")
 			.allowedMethods("OPTION", "GET", "POST", "PUT", "DELETE")
 			.allowedHeaders("*")
 			.allowCredentials(true)
